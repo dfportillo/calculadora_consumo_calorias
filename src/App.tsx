466d82d9accf3cb7af72c9911627d812f30e382c
Daniel Portillo <danielportillo303@gmail.com>
2024-06-22 11:26:02 -0400

    se agrega la opcion de dar reset a la App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Form from "./components/Form";
-import {  useReducer } from "react";
+import {  useEffect, useMemo, useReducer } from "react";
 import { activityReducer,initialState } from "./reducers/activity-reducers";
 import ActivityList from "./components/ActivityList";
 
@@ -9,13 +9,25 @@ function App() {
   
   const [state,dispatch] = useReducer(activityReducer,initialState);
 
+  useEffect(() => {
+    localStorage.setItem('activities',JSON.stringify(state.activities))
+  } ,[state.activities])
+
+  const restartOk = () => useMemo(() =>state.activities.length , [state.activities])
+
 
   return (
     <>
 
       <header className=" bg-lime-400 py-3">
-        <div className=" max-w-4xl mx-auto flex">
-          <h1 className=" text-center font-bold text-xl text-white uppercase">contador de calorias</h1>
+        <div className=" max-w-4xl mx-auto flex justify-between">
+          <h1 className=" text-center font-bold text-xl text-white uppercase place-items-center">contador de calorias</h1>
+          <button className=" first-letter:uppercase text-xl font-bold text-white w-40 h-10 bg-gray-700 hover:bg-gray-500 hover:border-4 border-black items-center rounded-lg disabled:opacity-10"
+          disabled={!restartOk()}
+          onClick={() => dispatch({type:'restart-app'})}
+          >
+            reiniciar app
+          </button>
         </div>
       </header>
       <section className=" bg-lime-600 py-20 px-5">
--- src/reducers/activity-reducers.ts
+++ src/reducers/activity-reducers.ts
@@ -1,13 +1,19 @@
-import { Activity,ActivityState } from "../types";
+import { Activity, ActivityState } from "../types";
 
 export type ActivityActions =
     { type: 'save-activity', payload: { newActivity: Activity } } |
     { type: 'set-activeId', payload: { id: Activity['id'] } } |
-    { type: 'delete-activity', payload: { id: Activity['id'] } } ;
+    { type: 'delete-activity', payload: { id: Activity['id'] } } |
+    { type: 'restart-app' }
 
+
+const localStorageActivities = (): Activity[] => {
+    const activities = localStorage.getItem('activities');
+    return activities ? JSON.parse(activities) : [];
+}
 
 export const initialState: ActivityState = {
-    activities: [],
+    activities: localStorageActivities(),
     activeId: '',
 };
 
@@ -18,36 +24,44 @@ export function activityReducer(
         // este codigo ejecuta los cambios del estado 
         // console.log(actions.payload.newActivity)
 
-        let uppdatedActivities:Activity[] = []
+        let uppdatedActivities: Activity[] = []
 
-        if(state.activeId){
-            uppdatedActivities = state.activities.map((activity) => activity.id === state.activeId ? actions.payload.newActivity :activity )
-        }else{
+        if (state.activeId) {
+            uppdatedActivities = state.activities.map((activity) => activity.id === state.activeId ? actions.payload.newActivity : activity)
+        } else {
             uppdatedActivities = [...state.activities, actions.payload.newActivity]
         };
-        
+
         return {
             ...state,
             activities: uppdatedActivities,
-            activeId:'' // para poder reiniciar el activedId 
+            activeId: '' // para poder reiniciar el activedId 
         };
     }
 
-    if(actions.type === 'set-activeId'){
+    if (actions.type === 'set-activeId') {
 
-        return{
+        return {
             ...state,
-            activeId:actions.payload.id
+            activeId: actions.payload.id
         }
     }
 
-    if(actions.type ==='delete-activity'){
+    if (actions.type === 'delete-activity') {
 
-        let uppdatedActivities = state.activities.filter( activity => activity.id !== actions.payload.id )
+        let uppdatedActivities = state.activities.filter(activity => activity.id !== actions.payload.id)
         return {
             ...state,
-            activities:uppdatedActivities
+            activities: uppdatedActivities
 
+        }
+    }
+
+    if (actions.type === 'restart-app') {
+
+        return {
+            activities: [],
+            activeId: ''
         }
     }
 
